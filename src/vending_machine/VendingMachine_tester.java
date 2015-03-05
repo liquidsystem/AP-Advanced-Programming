c6573b1ca60f3bc72713a692e2efeeb931ebf58b
Cameron Kurtz <cameronkurtz996@gmail.com>
2015-03-05 09:29:03 -0600

    update vending_machine.java

--- src/vending_machine/VendingMachine_tester.java
+++ src/vending_machine/VendingMachine_tester.java
@@ -1,14 +1,12 @@
 package vending_machine;
 
-public class VendingMachine_tester {
-	
-	public void createGUI()
-	{
-		
-	}
+import javax.swing.JFrame;
 
+public class VendingMachine_tester {
+
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		VendingMachine tester = new VendingMachine();
 		tester.getFoodsAndPrices();
 		tester.addOrChangeItem(0,0,"ayy",2.00);
@@ -20,6 +18,7 @@ public class VendingMachine_tester {
 		emptyTester.getMoneyEntered();
 		emptyTester.chooseFood(0,0);
 		emptyTester.getMoneyEntered();
+
 	}
 
 }
