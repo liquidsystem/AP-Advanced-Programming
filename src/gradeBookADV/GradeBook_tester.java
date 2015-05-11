166a14d6c877cd1539808fb4f823ad478c281b5d
Cameron Kurtz <cameronkurtz996@gmail.com>
2015-05-11 13:51:40 -0500

    Revert "ayyylmao"
    This reverts commit 2c8f74f08a1d391060deba26dbd78ba1e843681d.

--- src/gradeBookADV/GradeBook_tester.java
+++ src/gradeBookADV/GradeBook_tester.java
@@ -2,18 +2,17 @@ package gradeBookADV;
 
 public class GradeBook_tester {
 
-	public static void main(String[] args) 
-	{
-		
-		
-//		Student test = new Student("Cameron", 18, 12);
-//		System.out.println(test.getName());
-//		test.setCourse(0, "Gym");
-//		test.setCourse(1, "Art");
-//		test.setCourses(true);
-//		test.setGradingScale(0, .14, .86);
-//		System.out.println(test.getCourses());
-//		System.out.println(test.getGradingScale(0));
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student test = new Student("Cameron", 18, 12);
+//		test.createUI();
+		System.out.println(test.getName());
+		test.setCourse(0, "Gym");
+		test.setCourse(1, "Art");
+		test.setCourses(true);
+		test.setGradingScale(0, .14, .86);
+		System.out.println(test.getCourses());
+		System.out.println(test.getGradingScale(0));
 	}
 
 }
