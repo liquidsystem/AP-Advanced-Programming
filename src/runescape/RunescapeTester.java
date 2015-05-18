c864eb7e3b68931d70fd99d7a47f9e342b5ad963
Cameron Kurtz <cameronkurtz996@gmail.com>
2015-05-17 22:53:34 -0500

    Working on User Interface
    ...

--- src/runescape/RunescapeTester.java
+++ src/runescape/RunescapeTester.java
@@ -2,16 +2,25 @@ package runescape;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.swing.*;
 
 public class RunescapeTester {
 
 	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
+		createInterface();
 		RunescapeCharacter test = new RunescapeCharacter();
 		RunescapeCharacter test1 = new RunescapeCharacter("CammyJammy", new ArrayList<Integer>(Arrays.asList(4,46,45,54,43,52,48)));
 		RunescapeCharacter.createFile();
 		test1.writeStats();
 		RunescapeCharacter.readStats();
+	}
+	
+	public static void createInterface() {
+		JFrame f = new JFrame();
+		f.setTitle("Runescape Character Creator");
+		f.setSize(800,600);
+		f.setVisible(true);
 	}
 
 }
